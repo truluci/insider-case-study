974d190ebbb82fd5329ce120d068ef2a02b5ef61
truluci <yilmazmevlut397@gmail.com>
2026-05-19 19:29:23 +0300

    refactor: modularize CSS by extracting styles into dedicated layout, base, and component files. (Soft neo brutalisim)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './css/variables.css'
+import './css/base.css'
+import './css/layout.css'
+import './css/components.css'
 
 const app = createApp(App)
 app.mount('#app')
